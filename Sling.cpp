be85eda1c8df4e34ba4423091ad2e1bc05cf5ee4
Owen <992369991@qq.com>
2021-12-17 08:30:23 -0500

    Update Sling.cpp

--- Sling.cpp
+++ Sling.cpp
@@ -31,7 +31,7 @@ double Sum2(NumericVector &vec){  //Rcpp sugar
 }
 
 // [[Rcpp::export]]
-double Sum3(NumericVector &vec){  //Non point iterator
+double Sum3(NumericVector &vec){  //Non point iterator, pass by reference
   int n = vec.size();
   double total = 0;
   for (int i = 0; i < n; i++){
@@ -41,7 +41,7 @@ double Sum3(NumericVector &vec){  //Non point iterator
 }
 
 // [[Rcpp::export]]
-double Sum4(NumericVector vec){  //Non point iterator
+double Sum4(NumericVector vec){  //Non point iterator, pass by object
   int n = vec.size();
   double total = 0;
   for (int i = 0; i < n; i++){
@@ -182,7 +182,7 @@ List sling(NumericMatrix &X, NumericVector &y, NumericVector &lambdas,
           break;
         }
         Rcout << "loop3" << endl;
-        w_r = w_tilde;
+        w_r = clone(w_tilde);
         //Calculate z_r
         for (unordered_set<int>::iterator iter = U.begin(); iter != U.end(); iter++){//For pi in U
           double tmp = 0;
@@ -196,7 +196,7 @@ List sling(NumericMatrix &X, NumericVector &y, NumericVector &lambdas,
         
         double w_norm2 = 0;  //Because w_r = w_tilde
         while (!isConverge){
-          NumericVector w_tmp = w_tilde;
+          NumericVector w_tmp = clone(w_tilde);
           for (unordered_set<int>::iterator iter = U.begin(); iter != U.end(); iter++){//For pi in U
             double v_norm2 = 0;
             NumericVector upperBound(p);
@@ -236,7 +236,7 @@ List sling(NumericMatrix &X, NumericVector &y, NumericVector &lambdas,
         }
         
         isConverge = false;  //Reset the flag for next while loop
-        w_r = w_tilde;
+        w_r = clone(w_tilde);
         //Calculate z_r
         for (unordered_set<int>::iterator iter = U.begin(); iter != U.end(); iter++){//For pi in U
           double tmp = 0;
@@ -323,7 +323,7 @@ List sling(NumericMatrix &X, NumericVector &y, NumericVector &lambdas,
       }
     }
     
-    res( _ , k) = w_tilde;
+    res( _ , k) = clone(w_tilde);
     for (int m = 0; m < p; m++){
       if (w_tilde[m] != 0){
         P_k.push_back(m);
@@ -333,5 +333,199 @@ List sling(NumericMatrix &X, NumericVector &y, NumericVector &lambdas,
   
   NumericVector b = {1,2,3};
   return List::create(Named("beta") = b, Named("res", res));
+}
+
+
+// Standard Lasso
+double a(NumericMatrix &X, int i){
+  return mean(pow(X( _ , i), 2));
+}
+
+double z(NumericMatrix &X, NumericVector &y, NumericVector &w,
+          int i, int n, double w0){
+  double tot = 0;
+  for (int j = 0; j < n; j++){
+    double yhj = w0 + sum(w * X(j, _ )) - w[0]*X(j, i);
+    tot += (y[j] - yhj) * X(j, i);
+  }
+  return tot/n;
+}
+
+bool KKT(NumericMatrix &X, NumericVector &y, NumericVector &w,
+         int i, int n, double w0, double l){
+  bool res;
+  double e = 0.3;
+  double g = sum(X( _ , 1) * (y-(mvmult(X, w)+w0))) / (l*n);
+  if (w[i] > 0){
+    res = ((g>1-e) && (g<1+e));
+  }
+  else if (w[i] < 0){
+    res = ((g>-1-e) && (g<-1+e));
+  }
+  else{
+    res = ((g>-1-e) && (g<1+e));
+  }
+  return res;
+}
+
+// [[Rcpp::export]]
+List standardLasso(NumericMatrix &X, NumericVector &y, double l){
+  int n = X.nrow();
+  int p = X.ncol();
+  //double l = 100/(2*n);
+  double w0 = mean(y);
+  NumericVector w(p);
+  //w = w+1;
+  //DataFrame ws;  //part of result
+  NumericVector trainmse;
+  NumericVector testmse;
+  NumericVector wr(p);
+  NumericMatrix v(p);
+  
+  for (int i = 0; i < p; i++){
+    for (int j = 0; j < p; j++){
+      v(i, j) = sum(X( _ , i) * X( _ , j));
+    }
+  }
+  int nkkt = p;
+  int itr = 0;
+  while (nkkt > p/2){
+    itr += 1;
+    for (int i = 0; i < p; i++){
+      NumericVector wr = clone(w);
+      double zi = z(X, y, w, i, n, w0);
+      double ai = a(X, i);
+      if (zi > 1){
+        w[i] = (zi-1)/ai;
+      }
+      else if (zi < -l){
+        w[i] = (zi+1)/ai;
+      }
+      else{
+        w[i] = 0;
+      }
+      //NumericVector a = clone(w);
+      //ws.push_back(a);    //Super slow
+    }
+    nkkt = 0;
+    for (int i = 0; i < p; i++){
+      bool isKKT = KKT(X, y, w, i, n, w0, l);
+      if (!isKKT){
+        nkkt += 1;
+      }
+    }
+  }
+  return List::create(Named("w", w), /*Named("ws", ws),*/ Named("itr", itr));
+}
+
+
+// Sling python ver.
+double z_up(int i, int n, NumericVector &w, NumericVector &wr,
+            NumericMatrix &v, NumericVector &zr){
+  return w[i] - wr[i] + sum(pow(v(i, _ ), 2)) * sum(pow(w-wr, 2)) / n + zr[i];
+}
+
+double z_low(int i, int n, NumericVector &w, NumericVector &wr,
+             NumericMatrix &v, NumericVector &zr){
+  return w[i] - wr[i] - sum(pow(v(i, _ ), 2)) * sum(pow(w-wr, 2)) / n + zr[i];
+}
+
+void update(int i, int n, double l, NumericMatrix &X, NumericVector &y,
+              NumericVector &w, double w0){
+  double zi = z(X, y, w, i, n, w0);
+  double ai = a(X, i);
+  if (zi > l){
+    w[i] = (zi - l) / ai;
+  }
+  else if (zi < -l){
+    w[i] = (zi + l) / ai;
+  }
+  else{
+    w[i] = 0;
+  }
+}
+
+// [[Rcpp::export]]
+List sling2(NumericMatrix &X, NumericVector &y, double l){
+  int n = X.nrow();
+  int p = X.ncol();
+  //double l = 100/(2*n);
+  double w0 = mean(y);
+  NumericVector w(p);
+  //DataFrame ws;  //part of result
+  NumericVector trainmse;
+  NumericVector testmse;
+  NumericVector wr(p);
+  NumericMatrix v(p);
+  for (int i = 0; i < p; i++){
+    for (int j = 0; j < p; j++){
+      v(i, j) = sum(X( _ , i) * X( _ , j));
+    }
+  }
+  
+  NumericVector U;
+  bool loop = true;
+  int itr = 0;
+  int nkkt = p;
+  
+  while (nkkt > p/2){
+    itr += 1;
+    loop = false;
+    wr = w;
+    NumericVector zr;
+    NumericMatrix X_T = transpose(X);
+    zr = wr + mvmult(X_T, y)/n;
+    double err = 1e-4;
+    NumericVector prev(p);
+    prev = prev + R_PosInf;
+    while (is_true(all(abs(w-prev)>err))){
+      prev = w;  //Need to clone?
+      for (int j = 0; j < U.length(); j++){
+        int i = U[j];
+        double z_low_i = z_low(i, n, w, wr, v, zr);
+        double z_up_i = z_up(i, n, w, wr, v, zr);
+        if ((z_low_i > l) || (z_up_i < -l)){
+          update(i, n, l, X, y, w, w0);
+          //NumericVector a = clone(w);
+          //ws.push_back(a);    //Super slow
+        }
+      }
+    }
+    
+    wr = w;
+    zr = wr + mvmult(X_T, y)/n;
+    prev = prev*0 + R_PosInf;
+    while (is_true(all(abs(w-prev)>err))){
+      prev = w;  //Need to clone?
+      for (int j = 0; j < U.length(); j++){
+        int i = U[j];
+        double z_low_i = z_low(i, n, w, wr, v, zr);
+        double z_up_i = z_up(i, n, w, wr, v, zr);
+        if ((z_up_i > l) || (z_low_i < -l)){
+          update(i, n, l, X, y, w, w0);
+          //NumericVector a = clone(w);
+          //ws.push_back(a);    //Super slow
+        }
+        else{
+          w[i] = 0;
+          //NumericVector a = clone(w);
+          //ws.push_back(a);    //Super slow
+        }
+      }
+    }
+    
+    nkkt = 0;
+    for (int i = 0; i < p; i++){
+      bool isKKT = KKT(X, y, w, i, n, w0, l);
+      if (!isKKT){
+        loop = true;
+        nkkt += 1;
+        if (is_true(all(U != i))){
+          U.push_back(i);
+        }
+      }
+    }
+  }
+  return List::create(Named("w", w), /*Named("ws", ws),*/ Named("itr", itr));
 }
 
